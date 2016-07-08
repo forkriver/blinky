577e2efcb13ebd7246592ba3cee4d7e0d6d50ec3
Patrick Johanneson <pjohanneson@gmail.com>
2016-07-07 22:57:06 -0500

    Blinky core plugin files added

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+ * Plugin Name: Blinky
+ * Description: Let's make your WordPress site the heart of *your* social media network.
+ * Author Name: Patrick Johanneson
+ * Author URI: http://patrickjohanneson.com/
+ * Thanks: David Pensato (@pensato) for the idea
+ */
+
+require_once( 'class-blinky-core.php' );
